46b5f20ae14aad1160d7fe929faa327bede8196a
okrplay <32576280+okrplay@users.noreply.github.com>
2019-06-02 16:11:55 +0200

    Actix Base
    Signed-off-by: okrplay <32576280+okrplay@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate actix_web;
+extern crate redis;
+
+use actix_web::{server, App, HttpResponse};
+
 fn main() {
-    println!("Hello, world!");
+    // The actix server is started here.
+    // Mutiple Apps are used, one for the api, one for the websockets and one for the frontend.
+    server::new(|| {
+        vec![
+            App::new()
+                .prefix("/api")
+                .resource("/", |r| r.f(|_r| HttpResponse::Ok())),
+            App::new()
+                .prefix("/ws")
+                .resource("/", |r| r.f(|_r| HttpResponse::Ok())),
+            App::new()
+                .prefix("/")
+                .resource("/", |r| r.f(|_r| HttpResponse::Ok())),
+        ]
+    })
+    .bind("127.0.0.1:3467")
+    .unwrap()
+    .run();
 }
